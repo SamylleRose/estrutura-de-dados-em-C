8d0877b8a513ca8233e57f5bd55660efeacec021
SamylleRose <samylle1brito@gmail.com>
2024-11-19 09:55:45 -0300

    up

---
+++ Ed-I/tad_lista_encadeada/lista.c
@@ -0,0 +1,69 @@
+#include "lista.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+Lista *cria_lista()
+{
+  return NULL;
+}
+
+void libera_lista(Lista *li)
+{
+  Lista *aux;
+  while (li != NULL)
+  {
+    aux = li;
+    li = li->prox;
+    free(aux);
+  }
+}
+
+Lista *insere_lista_inicio(Lista *li, int i)
+{
+  Lista *novo = (Lista *)malloc(sizeof(Lista));
+  novo->info = i;
+  novo->prox = li;
+  return novo;
+}
+
+void mostrar_lista(Lista *li)
+{
+  Lista *aux = li;
+  while (aux != NULL)
+  {
+    printf("%d\n", aux->info);
+    aux = aux->prox;
+  }
+}
+
+Lista *remove_elemento(Lista *li, int i)
+{
+  Lista *aux = li;
+  Lista *ant = NULL;
+
+  while (aux != NULL && aux->info != i)
+  {
+
+    ant = aux;
+    aux = aux->prox;
+  }
+
+  if (ant == NULL)
+  {
+    li = aux->prox;
+    free(aux);
+  }
+
+  if (aux != NULL)
+  {
+    ant->prox = aux->prox;
+    free(aux);
+  }
+
+  else
+  {
+    printf("Elelmento não encontrado!");
+  }
+
+  return li;
+}
---
+++ Ed-I/tad_lista_encadeada/main.c
@@ -0,0 +1,18 @@
+
+#include "lista.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  Lista *li = cria_lista();
+  li = insere_lista_inicio(li, 10);
+  li = insere_lista_inicio(li, 20);
+  li = insere_lista_inicio(li, 30);
+  li = insere_lista_inicio(li, 40);
+  li = remove_elemento(li, 80);
+  mostrar_lista(li);
+  libera_lista(li);
+
+  return 0;
+}
