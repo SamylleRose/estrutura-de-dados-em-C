5493153566ad83795499d35b82c8c330f66d73ee
SamylleRose <samylle1brito@gmail.com>
2024-12-04 11:18:21 -0300

    commit desktop

---
+++ Ed-I/lista-encadeada-simples/lista.c
@@ -0,0 +1,75 @@
+#include "lista.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+Lista *cria_lista()
+{
+  return NULL;
+}
+
+void libera_lista(Lista *li)
+{
+  Lista *aux;
+  while (li != NULL)
+  {
+    aux = li;
+    li = li->prox;
+    free(aux);
+  }
+}
+
+Lista *insere_lista_inicio(Lista *li, int i)
+{
+  Lista *novo = (Lista *)malloc(sizeof(Lista));
+  novo->info = i;
+  novo->prox = li;
+  return novo;
+}
+
+void mostrar_lista(Lista *li)
+{
+  Lista *aux = li;
+  while (aux != NULL)
+  {
+    printf("%d\n", aux->info);
+    aux = aux->prox;
+  }
+}
+
+Lista *busca_elemento(Lista *li, int i)
+{
+  Lista *aux = li;
+  while (aux != NULL)
+  {
+    if (aux->info == i)
+      return aux;
+    aux = aux->prox;
+  }
+  return NULL;
+}
+Lista *remove_elemento(Lista *li, int i)
+{
+  Lista *ant = NULL;
+  Lista *aux = li;
+  while (aux != NULL && aux->info != i)
+  {
+    ant = aux;
+    aux = aux->prox;
+  }
+  // nao encontrou o elemento
+  if (aux == NULL)
+    return li;
+
+  // remove o elemento do inicio
+  if (ant == NULL)
+    li = aux->prox;
+  else
+    // remove o elemento do meio ou do fim
+    ant->prox = aux->prox;
+
+  // libera a memoria do elemento removido
+  free(aux);
+
+  // retorna a lista atualizada
+  return li;
+}
---
+++ Ed-I/lista-encadeada-simples/main.c
@@ -0,0 +1,28 @@
+#include "lista.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  Lista *li = cria_lista();
+  li = insere_lista_inicio(li, 10);
+  li = insere_lista_inicio(li, 20);
+  li = insere_lista_inicio(li, 30);
+  li = insere_lista_inicio(li, 40);
+  mostrar_lista(li);
+
+  if (busca_elemento(li, 22) != NULL)
+  {
+    printf("Elemento encontrado\n");
+  }
+  else
+  {
+    printf("Elemento nao encontrado\n");
+  }
+
+  li = remove_elemento(li, 20);
+  mostrar_lista(li);
+
+  libera_lista(li);
+  return 0;
+}
